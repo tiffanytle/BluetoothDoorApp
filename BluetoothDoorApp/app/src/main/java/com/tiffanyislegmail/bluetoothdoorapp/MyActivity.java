08be586198e826992b2d10df049c05c25f0b29b9
Tiffany <tiffanyisle@gmail.com>
2014-09-06 16:08:01 -0700

    adding myactivity.java

--- BluetoothDoorApp/app/src/main/java/com/tiffanyislegmail/bluetoothdoorapp/MyActivity.java
+++ BluetoothDoorApp/app/src/main/java/com/tiffanyislegmail/bluetoothdoorapp/MyActivity.java
@@ -1,9 +1,16 @@
 package com.tiffanyislegmail.bluetoothdoorapp;
 
 import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
 
 
 public class MyActivity extends Activity {
@@ -12,6 +19,11 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
@@ -32,5 +44,23 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        // fragmmentttt
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+            return rootView;
+        }
     }
 }
